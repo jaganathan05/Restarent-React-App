4c15ec819ef586baca6ae37a20b771d3eb8c58be
jaganathan05 <jaganathanv888@gmail.com>
2024-04-08 10:15:10 +0530

    create restarent summary

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvaliableMeals";
+import { Fragment } from "react";
+function Meals (){
+
+    return <Fragment>
+        <MealsSummary/>
+        <AvailableMeals/>
+    </Fragment>
+}
+
+export default Meals;
